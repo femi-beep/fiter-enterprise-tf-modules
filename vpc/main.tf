20c4d74cb49b11913aa4e7bad54f666edb1b532f
seyi.obaweya <seyi_obaweya@yahoo.com>
2023-08-14 21:59:14 +0200

    TSS-78: Migrate VPC Module to Remote Source

---
+++ vpc/main.tf
@@ -0,0 +1,40 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  name = "${var.customer}-${var.environment}-vpc"
+  azs  = var.azs ? var.azs : slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_tag = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  karpenter_tag = {
+    "karpenter.sh/discovery" = local.name
+    type                     = "private"
+  }
+  private_subnet_tags = var.enable_karpenter_autoscaler ? merge(local.private_tag, local.karpenter_tag) : local.private_tag
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 52)]
+
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+    type                     = "public"
+  }
+
+  private_subnet_tags = local.private_subnet_tags
+  tags                = var.common_tags
+}
---
+++ vpc/variable.tf
@@ -0,0 +1,36 @@
+variable "customer" {
+  description = "(Required) Name of Customer. ex: Fiter"
+  type        = string
+}
+
+variable "environment" {
+  description = "(Required) Environment e.g Dev, Stg, Prod"
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "(Required) VPC Cidr"
+  type = string
+}
+
+variable "azs" {
+  description = "(Optional) List of Availiability Zones"
+  type        = list(string)
+}
+
+variable "common_tags" {
+  description = "(Required) Resource Tag"
+  type        = map()
+}
+
+variable "enable_nat_gateway" {
+  description = "(Optional) Should be true if you want to provision NAT Gateways for each of your private networks"
+  default     = true
+  type        = bool
+}
+
+variable "single_nat_gateway" {
+  description = "(Optional) Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  default     = true
+  type        = bool
+}
