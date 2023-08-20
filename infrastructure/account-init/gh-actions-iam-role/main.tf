680f578f14a8b9a5580df949abeb15e399d69a43
seyi.obaweya <seyi_obaweya@yahoo.com>
2023-08-20 16:09:17 +0200

    TSS-78: Add Account Initialization Helper Modules

---
+++ infrastructure/account-init/gh-actions-iam-role/main.tf
@@ -0,0 +1,105 @@
+locals {
+  repo_url = [for value in var.repo_list : "repo:${value}:*"]
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [var.github_openidconnect_arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = local.repo_url
+    }
+  }
+}
+
+resource "aws_iam_policy" "deployment_role" {
+  name_prefix = "gh-deployment-policy-"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid : "AllowSpecifics"
+        Action = [
+          "lambda:*",
+          "apigateway:*",
+          "acm:*",
+          "ec2:*",
+          "rds:*",
+          "s3:*",
+          "sns:*",
+          "states:*",
+          "ssm:*",
+          "sqs:*",
+          "iam:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "cloudwatch:*",
+          "cloudfront:*",
+          "route53:*",
+          "ecr:*",
+          "logs:*",
+          "ecs:*",
+          "application-autoscaling:*",
+          "logs:*",
+          "events:*",
+          "es:*",
+          "kms:*",
+          "dynamodb:*",
+          "apprunner:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid : "DenySpecifics"
+        Action = [
+          "consolidatedbilling:*",
+          "invoicing:*",
+          "account:*",
+          "payments:*",
+          "budgets:*",
+          "tax:*",
+          "ce:*",
+          "cur:*",
+          "freetier:*",
+          "billing:*",
+          "config:*",
+          "directconnect:*",
+          "aws-marketplace:*",
+          "aws-marketplace-management:*",
+          "ec2:*ReservedInstances*",
+        ]
+        Effect   = "Deny"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "deployment_role" {
+  force_detach_policies = true
+  name_prefix           = var.deployment_role_name
+  assume_role_policy    = data.aws_iam_policy_document.github_actions_assume_role.json
+  tags = {
+    Name = var.deployment_role_name
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.deployment_role.name
+  policy_arn = aws_iam_policy.deployment_role.arn
+}
---
+++ infrastructure/account-init/gh-actions-iam-role/variables.tf
@@ -0,0 +1,11 @@
+variable "deployment_role_name" {
+  description = "The name of the Terraform IAM deployment role"
+  type        = string
+}
+
+variable "github_openidconnect_arn" {}
+
+variable "repo_list" {
+  type        = list(string)
+  description = "List of Repository to Grant Permission"
+}
