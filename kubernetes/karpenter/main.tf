2733b5134fb3e536c68782e26d150dfe07112a60
seyi.obaweya <seyi_obaweya@yahoo.com>
2023-08-25 02:54:40 +0200

    add karpenter helm release

---
+++ kubernetes/karpenter/main.tf
@@ -0,0 +1,42 @@
+locals {
+  provisioners = {
+    common = {
+      node_volume_size        = "100Gi"
+      node_volume_type        = "gp3"
+      node_encryption_enabled = true
+      # instance_category       = ["m", "r", "t"]
+    }
+  }
+}
+
+resource "helm_release" "karpenter" {
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  namespace           = "karpenter"
+  create_namespace    = true
+  repository_username = var.ecr_token_username
+  repository_password = var.ecr_token_password
+  chart               = "karpenter"
+  version             = "v0.29.2"
+  atomic              = true # wait for deployment to be ready
+  cleanup_on_fail     = true
+  values = [templatefile("${path.module}/files/values.yaml", {
+    KARPENTER_IAM_ROLE_ARN = var.karpenter_iam_role,
+    CLUSTER_NAME           = var.cluster_name,
+    CLUSTER_ENDPOINT       = var.cluster_endpoint,
+    INSTANCE_PROFILE_NAME  = var.instance_profile_name
+  })]
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  for_each = local.provisioners
+  yaml_body = templatefile("${path.module}/files/provisioners.yaml", {
+    provisioner_name        = each.key
+    aws_subnet_ids          = join(",", var.vpc_private_subnets)
+    aws_security_group_ids  = var.eks_node_security_group_id
+    node_volume_size        = lookup(each.value, "node_volume_size", "100Gi")
+    node_volume_type        = lookup(each.value, "node_volume_type", "gp3")
+    node_encryption_enabled = lookup(each.value, "node_encryption_enabled", true)
+    # instance_category       = lookup(each.value, "instance_category", ["m", "r", "t"])
+  })
+}
---
+++ kubernetes/karpenter/variables.tf
@@ -0,0 +1,31 @@
+variable "ecr_token_username" {
+  
+}
+
+variable "ecr_token_password" {
+  
+}
+
+variable "karpenter_iam_role" {
+  
+}
+
+variable "cluster_name" {
+  
+}
+
+variable "cluster_endpoint" {
+  
+}
+
+variable "instance_profile_name" {
+  
+}
+
+variable "eks_node_security_group_id" {
+  
+}
+
+variable "vpc_private_subnets" {
+  
+}
