104f2919d1b3b7525d04ece03ee27e857761d90e
seyi.obaweya <seyi_obaweya@yahoo.com>
2023-09-16 05:46:13 +0200

    Add ECR Modules

---
+++ infrastructure/ecr/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+  }
+}
+
+resource "aws_ecr_repository" "ecr" {
+  for_each = toset(var.registries_name)
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = "false"
+  }
+
+  image_tag_mutability = "MUTABLE"
+  name                 = each.value
+}
+
+
+variable "registries_name" {
+  description = "(Required) List of ECR Registries to be created"
+  default     = []
+}
