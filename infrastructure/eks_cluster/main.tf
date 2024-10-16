d44803f97bfcdb1759951bb71316681b7b0c743e
Napho <starnapho@gmail.com>
2024-10-16 10:42:31 +0300

    Check if cluster endpoint is not null

--- infrastructure/eks_cluster/main.tf
+++ infrastructure/eks_cluster/main.tf
@@ -36,7 +36,8 @@ data "aws_caller_identity" "current" {}
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+  cluster_ca_certificate = module.eks.cluster_certificate_authority_data != null ? base64decode(module.eks.cluster_certificate_authority_data) : ""
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
