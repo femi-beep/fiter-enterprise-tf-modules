28328b1e843af8d8edb16166884707dd93662f68
seyi <seyi.obaweya@getir.com>
2024-10-20 23:39:26 +0200

    update cluster module outputs

--- infrastructure/eks_cluster/output.tf
+++ infrastructure/eks_cluster/output.tf
@@ -33,3 +33,15 @@ output "karpenter_role_arn" {
 output "karpenter_instance_profile" {
   value = module.karpenter.instance_profile_name
 }
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
