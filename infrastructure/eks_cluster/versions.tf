486d44de8039d44c2e944376dd5929592fd145c0
seyi <seyi.obaweya@getir.com>
2025-03-23 21:07:48 +0100

    feat(eks): update eks module 20.0

--- infrastructure/eks_cluster/versions.tf
+++ infrastructure/eks_cluster/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.75"
+      version = "~> 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.30"
+      version = ">= 2.10"
     }
   }
 }
