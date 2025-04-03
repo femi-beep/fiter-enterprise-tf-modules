3d5bad56fb04ab07bca185bc745aafb150c84ca1
seyi <seyi.obaweya@getir.com>
2025-04-03 20:29:17 +0200

    feat(ecr): update ecr module

--- infrastructure/ecr/main.tf
+++ infrastructure/ecr/main.tf
@@ -1,13 +1,16 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47"
-    }
-  }
-}
+/*
+ * # AWS ECR Terraform Module
+ *
+ * This module provisions multiple [Amazon Elastic Container Registry (ECR)](https://aws.amazon.com/ecr/) repositories on AWS.
+ *
+ * Features of this module:
+ * - **ECR Repositories**: Automatically creates repositories for each name provided in the `registries_name` variable.
+ * - **Encryption**: Each repository is configured with AES256 encryption to ensure secure storage of container images.
+ * - **Image Scanning**: Image scanning is disabled by default (`scan_on_push = false`) but can be customized based on requirements.
+ *
+ * This module ensures security, scalability, and easy management of container image repositories for your workloads.
+ *
+ */
 
 resource "aws_ecr_repository" "ecr" {
   for_each = toset(var.registries_name)
@@ -22,10 +25,4 @@ resource "aws_ecr_repository" "ecr" {
   image_tag_mutability = "MUTABLE"
   name                 = each.value
   force_delete         = true
-}
-
-
-variable "registries_name" {
-  description = "(Required) List of ECR Registries to be created"
-  default     = []
 }
