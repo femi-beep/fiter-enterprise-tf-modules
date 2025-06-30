1d113dfd949728afe523adde782334890f109fdd
femi-beep <fhamid@fiter.io>
2025-06-30 02:44:57 -0500

    Update main.tf

--- infrastructure/eks_cluster/main.tf
+++ infrastructure/eks_cluster/main.tf
@@ -37,7 +37,7 @@ provider "kubernetes" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  version          =         "20.36.1"
   cluster_name    = local.cluster_name
   cluster_version = var.cluster_version
   subnet_ids      = var.subnets
