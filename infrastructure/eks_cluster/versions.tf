0ee2bfb848eea197d14b340131ffd993f7cd94e2
femi-beep <fhamid@fiter.io>
2025-06-30 02:42:25 -0500

    Update versions.tf

--- infrastructure/eks_cluster/versions.tf
+++ infrastructure/eks_cluster/versions.tf
@@ -4,23 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.95.0, < 6.0.0"
+      version = "~> 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = ">= 2.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9"
+      version = ">= 2.10"
     }
   }
 }
