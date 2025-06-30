0dd15bee302ec0d890302dea48176c9256db7f4b
femi-beep <fhamid@fiter.io>
2025-06-30 03:51:50 -0500

    Update main.tf

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+  version = "~> 5.17.0"
 
   name = local.name
   cidr = var.vpc_cidr
